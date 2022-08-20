9a4a696df0cca9c4631b78a0208f36c838229eb7
F.M. Panggabean <cichiomaru@users.noreply.github.com>
2022-08-20 17:50:21 +0700

    sound setting

--- Assets/Apps/Scripts/GATVirtualBooth/Game/UI/SettingButton.cs
+++ Assets/Apps/Scripts/GATVirtualBooth/Game/UI/SettingButton.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 namespace GATVirtualBooth.Game
 {
     public class SettingButton : MonoBehaviour
     {
-    
+        private Button Button => GetComponent<Button>();
+        private SettingUI SettingUI => FindObjectOfType<SettingUI>();
+
+
+        private void Awake()
+        {
+            Button.onClick.AddListener(ShowSetting);
+        }
+
+
+        private void ShowSetting()
+        {
+            SettingUI.Show();
+        }
     }
 }
